82384f23459f554aa598f8d5841822c0a91d26d1
akalanka345 <akalanka345@gmail.com>
2019-08-08 10:36:35 +0530

    header fix

--- common/Components/header.php
+++ common/Components/header.php
@@ -1,7 +1,7 @@
 <?php
-include 'php/dbh.inc.php';
-include 'php/services.inc.php';
-include 'php/viewservices.inc.php';
+//include 'php/dbh.inc.php';
+//include 'php/services.inc.php';
+//include 'php/viewservices.inc.php';
 ?>
 <script type="text/javascript" src="https://use.fontawesome.com/20bf8ce7c9.js"></script>    
     <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
